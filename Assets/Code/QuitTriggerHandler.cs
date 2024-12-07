92f4d605335ef7f1fc64e9506306c14fad364bfd
Amanda Nickerson <amnickerson3@gmail.com>
2024-12-06 20:55:49 -0400

    Done

--- Assets/Code/QuitTriggerHandler.cs
+++ Assets/Code/QuitTriggerHandler.cs
@@ -1,52 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;  
-using UnityEngine.SceneManagement;  
+using UnityEngine.SceneManagement;
 
 public class QuitTriggerHandler : MonoBehaviour
 {
-    public GameObject quitScreenPanel; 
-    public Button yesButton;           
-    public Button noButton;            
+    public GameObject player;
 
- 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
-        {
-            
-            quitScreenPanel.SetActive(true);
+        SceneManager.LoadScene(2);
 
-            
-        }
-    }
-
-   
-    void Start()
-    {
-       
-        quitScreenPanel.SetActive(false);
-
-        
-        yesButton.onClick.AddListener(QuitGame);
-        noButton.onClick.AddListener(CancelQuit);
-    }
-
-  
-    void QuitGame()
-    {
-        
-        Debug.Log("Quitting the game...");
-        Application.Quit();
-
-      
-    }
-
-    
-    void CancelQuit()
-    {
-       
-        quitScreenPanel.SetActive(false);
-
-
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
     }
 }
